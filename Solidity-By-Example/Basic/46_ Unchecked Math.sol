1fa064b729d139c9b2f6a9d78ce49123fc21e76b
Muhammad Zaid <zaid.aptechiic@gmail.com>
2025-04-22 01:34:29 +0500

    practice

---
+++ Solidity-By-Example/Basic/46_ Unchecked Math.sol
@@ -0,0 +1,170 @@
+/*
+Overflow and underflow of numbers in Solidity 0.8 throw an error. This can be disabled by using unchecked.
+Disabling overflow / underflow check saves gas.
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract UncheckedMath {
+    function add(uint256 x, uint256 y) external pure returns (uint256) {
+        // 22291 gas
+        // return x + y;
+
+        // 22103 gas
+        unchecked {
+            return x + y;
+        }
+    }
+
+    function add(uint8 x, uint8 y) external pure returns (uint8) {
+        // 22291 gas
+        // return x + y;
+
+        // 22103 gas
+        unchecked {
+            return x + y;
+        }
+    }
+
+    function add(uint16 x, uint16 y) external pure returns (uint16) {
+        // 22291 gas
+        // return x + y;
+
+        // 22103 gas
+        return x + y;
+    }
+
+    function add(uint64 x, uint64 y) external pure returns (uint64) {
+        // 22291 gas
+        // return x + y;
+
+        // 22103 gas
+        return x + y;
+    }
+
+    function sub(uint256 x, uint256 y) external pure returns (uint256) {
+        // 22329 gas
+        // return x - y;
+
+        // 22147 gas
+        unchecked {
+            return x - y;
+        }
+    }
+
+    function sub(uint32 x, uint32 y) external pure returns (uint32) {
+        // 22329 gas
+        // return x - y;
+
+        // 22147 gas
+        unchecked {
+            return x - y;
+        }
+    }
+
+    function sub(uint64 x, uint64 y) external pure returns (uint64) {
+        // 22329 gas
+        // return x - y;
+
+        // 22147 gas
+        return x - y;
+    }
+
+    function sub(uint128 x, uint128 y) external pure returns (uint128) {
+        // 22329 gas
+        // return x - y;
+
+        // 22147 gas
+        return x - y;
+    }
+
+    function sumOfCubes(uint256 x, uint256 y) external pure returns (uint256) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint256 x3 = x * x * x;
+            uint256 y3 = y * y * y;
+
+            return x3 + y3;
+        }
+    }
+
+    function sumOfCubes(uint8 x, uint8 y) external pure returns (uint8) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint8 x3 = x * x * x;
+            uint8 y3 = y * y * y;
+
+            return x3 + y3;
+        }
+    }
+
+    function sumOfCubes(uint32 x, uint32 y) external pure returns (uint32) {
+        // Wrap complex math logic inside unchecked
+        uint32 x3 = x * x * x;
+        uint32 y3 = y * y * y;
+
+        return x3 + y3;
+    }
+
+    function sumOfCubes(uint64 x, uint64 y) external pure returns (uint64) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint64 x3 = x * x * x;
+            uint64 y3 = y * y * y;
+
+            return x3 + y3;
+        }
+    }
+
+    function sumOfCubes(uint128 x, uint128 y) external pure returns (uint128) {
+        // Wrap complex math logic inside unchecked
+        uint128 x3 = x * x * x;
+        uint128 y3 = y * y * y;
+
+        return x3 + y3;
+    }
+
+    function power(uint256 x, uint256 y) external pure returns (uint256) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            return x * 10 ** y;
+        }
+    }
+
+    function power(uint8 x, uint8 y) external pure returns (uint8) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint8 x3 = x * x * x;
+            uint8 y3 = y * y * y;
+
+            return x3 + y3;
+        }
+    }
+
+    function power(uint32 x, uint32 y) external pure returns (uint32) {
+        // Wrap complex math logic inside unchecked
+        uint32 x3 = x * x * x;
+        uint32 y3 = y * y * y;
+
+        return x3 + y3;
+    }
+
+    function power(uint64 x, uint64 y) external pure returns (uint64) {
+        // Wrap complex math logic inside unchecked
+        unchecked {
+            uint64 x3 = x * x * x;
+            uint64 y3 = y * y * y;
+
+            return x3 + y3;
+        }
+    }
+
+    function power(uint128 x, uint128 y) external pure returns (uint128) {
+        // Wrap complex math logic inside unchecked
+        uint128 x3 = x * x * x;
+        uint128 y3 = y * y * y;
+
+        return x3 + y3;
+    }
+}

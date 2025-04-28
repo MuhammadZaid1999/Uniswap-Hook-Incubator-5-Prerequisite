71232971fe294a7461f67622b2a91705944a4bd8
Muhammad Zaid <zaid.aptechiic@gmail.com>
2025-04-29 01:48:49 +0500

    practice

--- Solidity-By-Example/2. Application Section/06_ ERC721(1).sol
+++ Solidity-By-Example/2. Application Section/06_ ERC721(1).sol
@@ -9,6 +9,7 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
+    using Strings for uint256;
 
     constructor(string memory name, string memory symbol) 
         ERC721(name, symbol) {}
@@ -20,5 +21,16 @@ contract MyNFT is ERC721 {
     function burn(uint256 id) external {
         require(msg.sender == ownerOf(id), "not owner");
         _burn(id);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "httpss://my-directory.com/";
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+
+        string memory baseURI = _baseURI();
+        return bytes(baseURI).length > 0 ? string.concat(baseURI, tokenId.toString(), ".json") : "";
     }
 }
--- Solidity-By-Example/2. Application Section/07_ ERC1155(1).sol
+++ Solidity-By-Example/2. Application Section/07_ ERC1155(1).sol
@@ -7,8 +7,10 @@ Example of ERC1155
 pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MyMultiToken is ERC1155 {
+    using Strings for uint256;
     
     constructor(string memory uri_) ERC1155(uri_){}
 
@@ -33,4 +35,18 @@ contract MyMultiToken is ERC1155 {
     {
         _burnBatch(msg.sender, ids, values);
     }
+
+    function setURI(string memory newuri) public {
+        _setURI(newuri);
+    }
+
+    function supportsInterface() public pure  returns (bytes4, bytes4, bytes4, bytes4) {
+        return (type(IERC1155).interfaceId, type(IERC1155MetadataURI).interfaceId, type(ERC165).interfaceId, type(IERC165).interfaceId);
+    }
+
+    function uri(uint256 id) public view override returns (string memory) {
+        string memory _uri = super.uri(id);
+        return bytes(_uri).length > 0 ? string.concat(_uri, id.toString(),".json") : "";
+    }
+
 }

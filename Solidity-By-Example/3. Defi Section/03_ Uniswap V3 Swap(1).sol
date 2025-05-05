9e23a547811b6409ac47dc648a0a0186b5201d3e
Muhammad Zaid <zaid.aptechiic@gmail.com>
2025-05-06 01:02:04 +0500

    practice

---
+++ Solidity-By-Example/3. Defi Section/03_ Uniswap V3 Swap(1).sol
@@ -0,0 +1,66 @@
+/*
+Uniswap V3 Single Hop Swap
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+address constant SWAP_ROUTER_02 = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
+address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+
+contract UniswapV3SingleHopSwap {
+    ISwapRouter private constant router = ISwapRouter(SWAP_ROUTER_02);
+    IERC20 private constant weth = IERC20(WETH);
+    IERC20 private constant dai = IERC20(DAI);
+
+    function swapExactInputSingleHop(uint256 amountIn, uint256 amountOutMin)
+        external
+    {
+        weth.transferFrom(msg.sender, address(this), amountIn);
+        weth.approve(address(router), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+            tokenIn: WETH,
+            tokenOut: DAI,
+            fee: 3000,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountIn: amountIn,
+            amountOutMinimum: amountOutMin,
+            sqrtPriceLimitX96: 0
+        });
+
+        router.exactInputSingle(params);
+    }
+
+    function swapExactOutputSingleHop(uint256 amountOut, uint256 amountInMax)
+        external
+    {
+        weth.transferFrom(msg.sender, address(this), amountInMax);
+        weth.approve(address(router), amountInMax);
+
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter
+            .ExactOutputSingleParams({
+            tokenIn: WETH,
+            tokenOut: DAI,
+            fee: 3000,
+            recipient: msg.sender,
+            deadline: block.timestamp,
+            amountOut: amountOut,
+            amountInMaximum: amountInMax,
+            sqrtPriceLimitX96: 0
+        });
+
+        uint256 amountIn = router.exactOutputSingle(params);
+
+        if (amountIn < amountInMax) {
+            weth.approve(address(router), 0);
+            weth.transfer(msg.sender, amountInMax - amountIn);
+        }
+    }
+}
